9a8ccdddaad0fd707f98afc0c8c46086b02163e0
Rhonda Warner <sunflair_2000@yahoo.com>
2021-08-10 17:45:33 -0500

    Added waiting room, navibar Will not compile

--- src/App.js
+++ src/App.js
@@ -6,67 +6,87 @@ import {
   Route,
   Switch,
 } from "react-router-dom";
-import { UserContext } from "./context";
+import { UserContext } from "./shared/context";
 import "./App.css";
-import ProtectedRoute from "./shared/ProtectedRoute";
+import ProtectedRoute from "./shared/components/ProtectedRoute";
 import LoginPage from "./components/LoginPage"
 import SignupPage from "./components/SignupPage"
-import GamePage from "./components/GamePage"
+import WaitingRoom from "./components/WaitingRoom"
+import GamePage from "./components/GamePage/GamePage"
+import Navibar from "./components/Navibar";
 
 function App() {
-const { username, logout } = useContext(UserContext);
+  const { username, logout } = useContext(UserContext);
 
-return (
-  <Router>
+  return (
+    <Router>
+      <div classname="container">
+        <>
+          <h3 className="m-3 d-flex justify-content-center">Lucky 7</h3><br />
+          <h5 className="m-3 d-flex justify-content-center">Battleship</h5>
+<Navibar/>
 
-    <NavLink
-      activeClassName="active"
-      to="/loginpage">
-      Login
-    </NavLink>
+          <NavLink
+            activeClassName="active"
+            to="/loginpage">
+            Login
+          </NavLink>
 
-    <NavLink
-      activeClassName="active"
-      to="/signuppage">
-      Signup
-    </NavLink>
+          <NavLink
+            activeClassName="active"
+            to="/signuppage">
+            Signup
+          </NavLink>
 
-    <NavLink
-      activeClassName="active"
-      to="/gamepage">
-      GameBoard
-    </NavLink>
+          <NavLink
+            activeClassName="active"
+            to="/gamepage">
+            GameBoard
+          </NavLink>
 
-    <NavLink
-      to="/login"
-      onClick={() => {
-        logout();
-      }}>
-      Logout
-    </NavLink>
+          <NavLink
+            activeClassName="active"
+            to="/waitingroom">
+            WaitingRoom
+          </NavLink>
 
-    <main>
-      <Switch>
-        <ProtectedRoute path="/loginpage" reqUser={false}>
-          <LoginPage />
-        </ProtectedRoute>
+          <NavLink
+            to="/login"
+            onClick={() => {
+              logout();
+            }}>
+            Logout
+          </NavLink>
 
-        <ProtectedRoute path="/signuppage" reqUser={false}>
-          <SignupPage />
-        </ProtectedRoute>
+          <main>
+            <Switch>
+              <ProtectedRoute path="/loginpage" reqUser={false}>
+                <LoginPage />
+              </ProtectedRoute>
 
-        <ProtectedRoute path="/gamepage" reqUser={true}>
-          <GamePage />
-        </ProtectedRoute>
+              <ProtectedRoute path="/signuppage" reqUser={false}>
+                <SignupPage />
+              </ProtectedRoute>
 
-        <Route path="*">
-          <Redirect to="/login" />
-        </Route>
+              <ProtectedRoute path="/gamepage" reqUser={true}>
+                <GamePage />
+              </ProtectedRoute>
 
-      </Switch>
-    </main>
-  </Router>
-);
+              <ProtectedRoute path="/waitingroom" reqUser={true}>
+                <WaitingRoom />
+              </ProtectedRoute>
+
+              <Route path="*">
+                <Redirect to="/login" />
+              </Route>
+
+            </Switch>
+          </main>
+        </>
+      </div>
+    </Router>
+
+  );
 }
 
 export default App;
---
+++ src/components/Navibar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { Navbar, Nav } from "react-bootstrap";
+
+export default function Navibar() {
+
+	return (
+		<>
+			<Navbar bg="dark">
+				<Navbar.Toggle aria-controls="basic-navbar-nav" />
+				<Navbar.Collapse id="basic-navbar-nav">
+					<Nav>
+						< NavLink className="d-inline p-2 bg-dark text-white" to="/">Log In</NavLink>
+						< NavLink className="d-inline p-2 bg-dark text-white" to="/waitingroom">Waiting Room</NavLink>
+					</Nav>
+				</Navbar.Collapse>
+			</Navbar>
+		</>
+	);
+}
