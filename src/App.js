cf17aae62ff7af40701eb780281d357b7a677a96
Rhonda Warner <sunflair_2000@yahoo.com>
2021-08-10 15:51:49 -0500

    routing links started

--- src/App.js
+++ src/App.js
@@ -1,5 +1,72 @@
+import React, { useContext } from "react";
+import {
+  BrowserRouter as Router,
+  NavLink,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
+import { UserContext } from "./context";
+import "./App.css";
+import ProtectedRoute from "./shared/ProtectedRoute";
+import LoginPage from "./components/LoginPage"
+import SignupPage from "./components/SignupPage"
+import GamePage from "./components/GamePage"
+
 function App() {
-  return <div></div>;
+const { username, logout } = useContext(UserContext);
+
+return (
+  <Router>
+
+    <NavLink
+      activeClassName="active"
+      to="/loginpage">
+      Login
+    </NavLink>
+
+    <NavLink
+      activeClassName="active"
+      to="/signuppage">
+      Signup
+    </NavLink>
+
+    <NavLink
+      activeClassName="active"
+      to="/gamepage">
+      GameBoard
+    </NavLink>
+
+    <NavLink
+      to="/login"
+      onClick={() => {
+        logout();
+      }}>
+      Logout
+    </NavLink>
+
+    <main>
+      <Switch>
+        <ProtectedRoute path="/loginpage" reqUser={false}>
+          <LoginPage />
+        </ProtectedRoute>
+
+        <ProtectedRoute path="/signuppage" reqUser={false}>
+          <SignupPage />
+        </ProtectedRoute>
+
+        <ProtectedRoute path="/gamepage" reqUser={true}>
+          <GamePage />
+        </ProtectedRoute>
+
+        <Route path="*">
+          <Redirect to="/login" />
+        </Route>
+
+      </Switch>
+    </main>
+  </Router>
+);
 }
 
 export default App;
