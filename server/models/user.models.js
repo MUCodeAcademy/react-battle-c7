a6c438e386dbb9f3500fc58f60eea526f401e3d4
Michael Zimmerman <mikezne@gmail.com>
2021-08-10 10:21:35 -0500

    adds signup model query

--- server/models/user.models.js
+++ server/models/user.models.js
@@ -1,1 +1,27 @@
 const bcrypt = require("bcrypt");
+const { pool } = require("../config/pg.conf");
+const { v4: uuidv4 } = require("uuid");
+
+async function signup(res, username, password) {
+  let json = { data: null, success: false, error: null };
+  try {
+    const users = await pool.query("SELECT * FROM users WHERE username = ?", [
+      username,
+    ]);
+    if (users.length !== 0) {
+      json = { ...json, error: "Username already taken" };
+    } else {
+      const hashed = await bcrypt.hash(password, 10);
+      const uuid = uuidv4();
+      await pool.query(
+        "INSERT INTO users (password, username, uuid) VALUES (?,?,?)",
+        [hashed, username, uuid]
+      );
+      json = { ...json, success: true };
+    }
+  } catch (err) {
+    json = { ...json, error: "something went wrong" };
+  } finally {
+    return res.send(json);
+  }
+}
