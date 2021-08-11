8122ac5aed7f8fa542a71f19007191cf5aa40ec6
keithawess <61606760+keithwess@users.noreply.github.com>
2021-08-11 13:58:48 -0500

    Fixes merge errors

--- src/App.js
+++ src/App.js
@@ -17,12 +17,12 @@ import Navibar from "./components/Navibar";
 import { Container } from "react-bootstrap";
 
 function App() {
-  // const { username, logout } = useContext(UserContext);
+  const { username, logout } = useContext(UserContext);
 
   return (
     <Router>
 
-      <div className="container">
+      <Container className="container">
         <>
           <h3 className="m-3 d-flex justify-content-center">Lucky 7</h3><br />
           <h5 className="m-3 d-flex justify-content-center">Battleship</h5>
@@ -54,13 +54,13 @@ function App() {
             WaitingRoom
           </NavLink>
 
-          {/* <NavLink
+          <NavLink
             to="/login"
             onClick={() => {
               logout();
             }}>
             Logout
-          </NavLink> */}
+          </NavLink>
 
           <main>
             <Switch>
