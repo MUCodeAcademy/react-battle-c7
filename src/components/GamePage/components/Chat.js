31f9f74cfac5cabb0ce39e4877e59d7e0749931a
keithawess <61606760+keithwess@users.noreply.github.com>
2021-08-12 11:52:15 -0500

    Adds text box and input box
    Co-authored-by: trevorrjacobson <trevorrjacobson@users.noreply.github.com>

--- src/components/GamePage/components/Chat.js
+++ src/components/GamePage/components/Chat.js
@@ -1,7 +1,24 @@
 import React from "react";
+import useSocket from "../../../shared/hooks/useSocket";
+import { Card, Form, Button, Container, ListGroup } from "react-bootstrap";
 
 function Chat() {
-  return <div className="chat">This is the Chat Page</div>;
+  // const { sendChat, messages } = useSocket;
+
+  const messages = ["Hi", "Hey", "ga", "gaasdg", "ate", "teatsdt", "asdgadsg"];
+
+  return (
+    <Container>
+      <Card className="chat">
+        {messages.map((msg) => {
+          return <Card.Text>{msg}</Card.Text>;
+        })}
+      </Card>
+      <Form.Group id="">
+        <Form.Control type="" />
+      </Form.Group>
+    </Container>
+  );
 }
 
 export default Chat;
