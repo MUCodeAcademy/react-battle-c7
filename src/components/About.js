07692b61cc04ffd889bbfefcb65ebca73978bf91
DarcyJorgensen <darcy.jorgensen_23@yahoo.com>
2021-08-12 11:38:37 -0500

    initial text for Intro and Game play

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,18 @@
 import React from "react";
 
-
 export default function About() {
-
-	return (
-
-
-		<>
-		<p>About Text Here</p>
-		</>
-	);
+  return (
+    <>
+      <p>
+        Welcome to Lucky 7 Battleship! Created by the 7th cohort of Midland Code
+        Academy. Sign up and Login or if you’re already signed up, log in.
+        Select start game to retrieve a play code or join game to enter a code
+        from the host. Select the ship you want to place on your grid and do
+        this for all four ships. Confirm/Ready (button) to start game. Click on
+        Opponent’s grid to select a hit or miss. White peg is a miss. Red peg is
+        a hit. Total of 14 hits, wins the game. Keep watch on the Scoreboard for
+        your stats. Do you feel lucky?
+      </p>
+    </>
+  );
 }
-
