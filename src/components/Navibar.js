9f47d679e5c77aa7e9766411a25f97f75f6844bc
Rhonda Warner <sunflair_2000@yahoo.com>
2021-08-12 10:36:38 -0500

    added logo image

--- src/components/Navibar.js
+++ src/components/Navibar.js
@@ -1,54 +1,41 @@
-import React from "react";
+import React, {useState} from "react";
+import { Link } from "react-router-dom";
+import {Navbar, Container, Nav} from "react-bootstrap"
+
+
+
 export default function Navibar() {
+
 	return (
-		<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-			<div class="container-fluid">
-				<a class="navbar-brand">
-					Lucky 7   Battleship
-				</a>
-				<button
-					class="navbar-toggler"
-					type="button"
-					data-bs-toggle="collapse"
-					data-bs-target="#navbarNavDropdown"
-					aria-controls="navbarNavDropdown"
-					aria-expanded="false"
-					aria-label="Toggle navigation"
-				>
-					<span class="navbar-toggler-icon"></span>
-				</button>
-				<div class="collapse navbar-collapse" id="navbarNavDropdown">
-					<ul class="navbar-nav">
-						<li class="nav-item">
-							<a class="nav-link active" aria-current="page" href="/loginpage">
-								Login
-							</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="/waitingroom">
-								Waiting Room
-							</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="/about">
-								About
-							</a>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</nav>
-		// <>
-		//    <Navbar bg="dark" expands="lg">
-		//                <Navbar.Collapse id="basic-navbar-nav">
-		//                            <Nav>
-		//                                        < NavLink className="d-inline p-2 bg-dark text-white justify-content-center" to="/">Log In</NavLink>
-		//                                        < NavLink className="d-inline p-2 bg-dark text-white" to="/waitingroom">Waiting Room</NavLink>
-		//                            </Nav>
-		//                </Navbar.Collapse>
-		//    </Navbar>
-		// </>
-	);
+	<>
+<br />
+<Navbar fixed="top" />
+  <Navbar bg="dark" variant="dark">
+    <Container>
+      <Navbar.Brand>
+        <img
+          src=".../public/assets/7battle.png"
+          alt=""
+        color="white"
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+        />{' '}
+     Lucky 7 Battleship
+      </Navbar.Brand>
+<Nav className="me-auto">
+      <Nav.Link href="/login">Log In</Nav.Link>
+      <Nav.Link href="/waitingroom">Sign Up</Nav.Link>
+      <Nav.Link href="#waitingroom">Waiting Room</Nav.Link>
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    </Nav>
+       </Container>
+  </Navbar>
+</>
+
+		);
 }
 
 
