659933a718dbbaf7535a57267d2c8fc6bb80ee30
keithawess <61606760+keithwess@users.noreply.github.com>
2021-08-13 15:35:38 -0500

    Adds toggle to scoreboard

--- src/components/GamePage/components/ScoreBoard.js
+++ src/components/GamePage/components/ScoreBoard.js
@@ -1,114 +1,7 @@
-import { Card } from "@material-ui/core";
 import React, { useState } from "react";
 
-
-const arr = [
-  { player: false, hit: false, ship: true, coordinate: "" },
-  { player: false, hit: true, ship: true, coordinate: "" },
-  { player: false, hit: false, ship: true, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: true, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: true, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: false, ship: false, coordinate: "" },
-  { player: false, hit: true, ship: true, coordinate: "" },
-  { player: false, hit: true, ship: true, coordinate: "" },
-  { player: false, hit: true, ship: true, coordinate: "" },
-  { player: false, hit: true, ship: true, coordinate: "" },
-  { player: false, hit: true, ship: true, coordinate: "" },
-];
-
-
-
 function ScoreBoard() {
-
+  const [boatsToggle, setBoatsToggle] = useState(false);
   const [isActive2, setActive2] = useState("false");
   const [isActive3, setActive3] = useState("false");
   const [isActive4, setActive4] = useState("false");
@@ -127,60 +20,73 @@ function ScoreBoard() {
     setActive5(!isActive5);
   };
 
-  let boatsReady = true
-
+  let boatsReady = true;
 
   return (
     <>
-      <h5 className="title"> Place Boats</h5>
+    {boatsToggle && 
+      <div>
+        <h5 className="title"> Place Boats</h5>
 
-      <div className="shell">
-      <div className="flexship">
+        <div className="shell">
+          <div className="flexship">
+            <div className={isActive2 ? "boat2" : "boat2v"}>
+              <button onClick={boat2Toggle}></button>
+            </div>
 
-        <div className={isActive2 ? "boat2" : "boat2v"}>
+            <div className={isActive3 ? "boat3" : "boat3v"}>
+              <button onClick={boat3Toggle}></button>
+            </div>
 
-          <button onClick={boat2Toggle}></button>
+            <div className={isActive4 ? "boat4" : "boat4v"}>
+              <button onClick={boat4Toggle}></button>
+            </div>
+
+            <div className={isActive5 ? "boat5" : "boat5v"}>
+              <button onClick={boat5Toggle}></button>
+            </div>
+          </div>
         </div>
-
-        <div className={isActive3 ? "boat3" : "boat3v"}>
-
-          <button onClick={boat3Toggle}></button>
-        </div>
-
-        <div className={isActive4 ? "boat4" : "boat4v"}>
-
-          <button onClick={boat4Toggle}></button>
-        </div>
-
-        <div className={isActive5 ? "boat5" : "boat5v"}>
-
-          <button onClick={boat5Toggle}></button>
-        </div></div>
-
-       <div className="rbtn"><button className="readybtn" onClick={boatsReady}></button>
-      </div> </div>
-
+      </div>}
+      <div className="rbtn">
+        <button
+          className="readybtn"
+          onClick={() => {
+            setBoatsToggle(!boatsToggle);
+          }}
+        ></button>
+      </div>
       {/* conditional render for active play */}
 
       <h5 className="title">Score Board</h5>
 
-<div className="shell2">
+      <div className="shell2">
+        <div className="ht-ms">
+          <div>Hits:</div>
+          <div>Miss:</div>
+        </div>
 
-<div className="ht-ms">
-      <div>Hits:</div>
-      <div>Miss:</div></div>
-
-      <div className="shipbox">
-        <div><img className="size2" src="./assets/7battle.png" alt="ship 2" /><div className="ship-2">2</div></div>
-        <div><img className="size3" src="./assets/7battle.png" alt="ship 3" /><div className="ship-3">3</div></div>
-        <div><img className="size4" src="./assets/7battle.png" alt="ship 4" /><div className="ship-4">4</div></div>
-        <div><img className="size5" src="./assets/7battle.png" alt="ship 5" /><div className="ship-4">5</div></div>
-      </div></div>
-
-
+        <div className="shipbox">
+          <div>
+            <img className="size2" src="/assets/7battle.png" alt="ship 2" />
+            <div className="ship-2">2</div>
+          </div>
+          <div>
+            <img className="size3" src="/assets/7battle.png" alt="ship 3" />
+            <div className="ship-3">3</div>
+          </div>
+          <div>
+            <img className="size4" src="/assets/7battle.png" alt="ship 4" />
+            <div className="ship-4">4</div>
+          </div>
+          <div>
+            <img className="size5" src="/assets/7battle.png" alt="ship 5" />
+            <div className="ship-4">5</div>
+          </div>
+        </div>
+      </div>
     </>
   );
-
 }
 
 export default ScoreBoard;
