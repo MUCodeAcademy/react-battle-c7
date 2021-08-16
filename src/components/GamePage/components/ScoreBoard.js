3abec154f009c0ddc07753656851cb956613b2ed
Rhonda Warner <sunflair_2000@yahoo.com>
2021-08-16 16:45:38 -0500

    working on toggle for ship sunk isSunk

--- src/components/GamePage/components/ScoreBoard.js
+++ src/components/GamePage/components/ScoreBoard.js
@@ -68,23 +68,46 @@ function ScoreBoard() {
 
         <div className="shipbox">
           <div>
-            <img className="size2" src="/assets/7battle.png" alt="ship 2" />
+            
             <div className="ship-2">2</div>
           </div>
           <div>
-            <img className="size3" src="/assets/7battle.png" alt="ship 3" />
             <div className="ship-3">3</div>
           </div>
           <div>
-            <img className="size4" src="/assets/7battle.png" alt="ship 4" />
             <div className="ship-4">4</div>
           </div>
           <div>
-            <img className="size5" src="/assets/7battle.png" alt="ship 5" />
             <div className="ship-4">5</div>
-          </div>
-        </div>
-      </div>
+           {boatsToggle && 
+      
+        
+
+        <div className="shell">
+          <div className="flexship">
+            <div className={isActive2 ? "boat2" : "boat2v"}>
+              <button onClick={boat2Toggle}></button>
+            </div>
+
+            <div className={isActive3 ? "boat3" : "boat3v"}>
+              <button onClick={boat3Toggle}></button>
+            </div>
+
+            <div className={isActive4 ? "boat4" : "boat4v"}>
+              <button onClick={boat4Toggle}></button>
+            </div>
+
+            <div className={isActive5 ? "boat5" : "boat5v"}>
+              <button onClick={boat5Toggle}></button>
+            </div>
+          </div> </div>
+       
+      
+
+
+
+       
+} </div> </div> </div>
     </>
   );
 }
