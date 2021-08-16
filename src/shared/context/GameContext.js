cf1793d42ae99ae174eab8df0801d812a40d9f95
keithawess <61606760+keithwess@users.noreply.github.com>
2021-08-16 11:30:10 -0500

    Adds base array

--- src/shared/context/GameContext.js
+++ src/shared/context/GameContext.js
@@ -1,21 +1,40 @@
-import React from "react";
+import React, {useCallback, useEffect, useState} from "react";
 
 export const GameContext = createContext(null);
 
 
 export function GameProvider(props) {
-    const userDate= [{}];
-    const opponentData = [{}];
+    const [userData, setUserData] = useState([]);
+    const [opponentData, setOpponentData] = useState([]);
     let ready = false;
     let turn = false;
     let userHit = 0;
     let opponentHit = 0;
     let totalGuesses = 0;
 
+    useEffect(()=>{
+      userData.fill({player: true, hit: false, ship: false}, 0, 99);
+      opponentData.fill({player: false, hit: false, ship: false}, 0, 99);
+      setUserData(curr => [...curr]);
+      setOpponentData(curr => [...curr]);
+    },[])
 
-    const placeBoat = ((int)=>{
 
-    })
+    const placeBoat = useCallback( (coordinates, int, orientation) => {
+      for(let i = 0; i < int; i++)
+      {
+        if(orientation === 'h')
+        {
+          userData[coordinates + i].ship = true;
+        }
+        else
+        {
+          userData[coordinates + (i * 10)].ship = true;
+        }
+      }
+    });
+
+
    const ready = (()=>{
         
     })
