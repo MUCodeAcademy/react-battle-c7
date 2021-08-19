132a35004d5a2e5d614265fa822e02b8a2a44ebd
Rhonda Warner <sunflair_2000@yahoo.com>
2021-08-19 14:53:40 -0500

    updated middleware

--- server/models/user.models.js
+++ server/models/user.models.js
@@ -1,52 +1,104 @@
 const bcrypt = require("bcrypt");
-const pool = require("../config/pg.conf");
+const query = require("../config/pg.conf");
 const { v4: uuidv4 } = require("uuid");
 
 async function signup(res, username, password) {
+  //! check if username in use
   let json = { data: null, success: false, error: null };
   try {
-    const users = await pool.query("SELECT * FROM users WHERE username = $1", [
+    const users = await query("SELECT * FROM users WHERE username = ?", [
       username,
     ]);
-    if (users.rows.length !== 0) {
-      json = { ...json, error: "Username already taken" };
+    if (users.length !== 0) {
+      json.error = "Username already taken";
     } else {
       const hashed = await bcrypt.hash(password, 10);
       const uuid = uuidv4();
-      await pool.query(
-        "INSERT INTO users (password, username, uuid) VALUES ($1,$2,$3)",
+      await query(
+        "INSERT INTO users (password, username, uuid) VALUES (?,?,?)",
         [hashed, username, uuid]
       );
       json = { ...json, success: true };
     }
   } catch (err) {
-    console.log(err)
-    json = { ...json, error: "something went wrong" };
+    json.error = "Something went wrong?";
   } finally {
     return res.send(json);
   }
 }
 
-async function login(res, username, password) {
-  let json = { data: null, success: false, error: null };
+async function login(username, password) {
+  let json = { data: null, error: null };
   try {
-    const users = await pool.query("SELECT * FROM users WHERE username = $1", [
+    const users = await query("SELECT * FROM users WHERE username = ?", [
       username,
     ]);
-
-    const user = users.rows[0] || { password: "1234" };
-    const match = await bcrypt.compare(password, user.password);
-    if (match) {
-      json = { ...json, success: true, data: { username, uuid: user.uuid } };
+    const user = users[0] || { password: "1234" };
+    const matches = await bcrypt.compare(password, user.password);
+    if (matches) {
+      json = { ...json, data: { username, uuid: user.uuid } };
     } else {
-      json = { ...json, error: "Invalid username and/or password" };
+      json.error = "Invalid username / password";
     }
   } catch (err) {
-    console.log(err);
-    json = { ...json, error: "something went wrong" };
+    json.error = "Something went wrong?";
+  } finally {
+    return json;
+  }
+}
+
+async function getByUserID(uuid) {
+  let json = { error: null, data: null };
+  try {
+    const users = await query(
+      "SELECT id, username, uuid FROM users WHERE uuid = ?",
+      [uuid]
+    );
+    if (users.length === 0) {
+      json.error = "No user found";
+    } else {
+      json = { ...json, data: users[0] };
+    }
+  } catch (err) {
+    json.error = "Something went wrong?";
+  } finally {
+    return json;
+  }
+}
+
+async function getByUsername(res, username) {
+  let json = { success: false, error: null, data: null };
+  try {
+    const users = await query(
+      "SELECT id, username, uuid FROM users WHERE username = ?",
+      [username]
+    );
+    if (users.length === 0) {
+      json.error = "No user found";
+    } else {
+      json = { ...json, success: true, data: users[0] };
+    }
+  } catch (err) {
+    json.error = "Something went wrong?";
   } finally {
     return res.send(json);
   }
 }
 
-module.exports = { signup, login };
+async function getAllUsers(res) {
+  let json = { success: false, error: null, data: null };
+  try {
+    const users = await query("SELECT id, username, uuid FROM users");
+    if (users.length === 0) {
+      json.error = "No users found";
+    } else {
+      json = { ...json, success: true, data: users };
+    }
+  } catch (err) {
+    json.error = "Something went wrong?";
+  } finally {
+    return res.send(json);
+  }
+}
+
+module.exports = { signup, login, getAllUsers, getByUserID, getByUsername };
