77e903c1a3316516491124336e788467342c5113
Zachary-Roland <zsolo95@gmail.com>
2021-08-19 10:31:06 -0500

    corrects mismerge

--- src/components/GamePage/components/ScoreBoard.js
+++ src/components/GamePage/components/ScoreBoard.js
@@ -2,20 +2,13 @@ import React, { useState, useContext } from "react";
 import { Button } from "react-bootstrap";
 import { GameContext } from "../../../shared/context/GameContext";
 import { useParams } from "react-router-dom";
-
-function ScoreBoard({
-  setBoatToPlace,
-  setBoatOrient,
-  sendBoatsReady,
-  boatOrient,
-}) {
+function ScoreBoard({ setBoatOrient, boatOrient }) {
   const {
     userHit,
     oppHit,
     totalGuesses,
     setUserBoatsReady,
     gameActive,
-    userData,
     currentShip,
     oppShips,
   } = useContext(GameContext);
@@ -24,116 +17,71 @@ function ScoreBoard({
   const [isActive4, setActive4] = useState(false);
   const [isActive5, setActive5] = useState(false);
   let misses = totalGuesses - userHit;
-
   const { room } = useParams();
-  const boat2Toggle = () => {
+  const boatToggle = () => {
+    setActive5(!isActive5);
+    setActive4(!isActive4);
+    setActive3(!isActive3);
     setActive2(!isActive2);
   };
-  const boat3Toggle = () => {
-    setActive3(!isActive3);
-  };
-  const boat4Toggle = () => {
-    setActive4(!isActive4);
-  };
-  const boat5Toggle = () => {
-    setActive5(!isActive5);
-  };
-
-  let userBoatsReady = true;
-
   return (
     <>
       {!gameActive && (
         <div>
           <h5 className="title"> Place Boats</h5>
-
+          <h6>
+            (Click on the button to change orientation of ships)
+            <button
+              onClick={() => {
+                if (boatOrient === "v") {
+                  setBoatOrient("h");
+                  boatToggle();
+                } else {
+                  setBoatOrient("v");
+                  boatToggle();
+                }
+              }}
+            >
+              Swap Orientation
+            </button>
+          </h6>
           <div className="shell">
             <div className="flexship">
-              <div className={isActive2 ? "boat2" : "boat2v"}>
-                <button
-                  onClick={() => {
-                    setBoatToPlace(2);
-                    if (isActive2) {
-                      setBoatOrient("v");
-                    } else {
-                      setBoatOrient("h");
-                    }
-                    boat2Toggle();
-                  }}
-                ></button>
-              </div>
-
-              <div className={isActive3 ? "boat3" : "boat3v"}>
-                <button
-                  onClick={() => {
-                    setBoatToPlace(3);
-                    if (isActive3) {
-                      setBoatOrient("v");
-                    } else {
-                      setBoatOrient("h");
-                    }
-                    boat3Toggle();
-                  }}
-                ></button>
-              </div>
-
-              <div className={isActive4 ? "boat4" : "boat4v"}>
-                <button
-                  onClick={() => {
-                    setBoatToPlace(4);
-                    if (isActive4) {
-                      setBoatOrient("v");
-                    } else {
-                      setBoatOrient("h");
-                    }
-                    boat4Toggle();
-                  }}
-                ></button>
-              </div>
-
-              <div className={isActive5 ? "boat5" : "boat5v"}>
-                <button
-                  onClick={() => {
-                    setBoatToPlace(5);
-                    if (isActive5) {
-                      setBoatOrient("v");
-                    } else {
-                      setBoatOrient("h");
-                    }
-                    boat5Toggle();
-                  }}
-                ></button>
-              </div>
+              <div
+                className={`${isActive5 ? "boat5" : "boat5v"} ${
+                  currentShip < 5 ? "bg-white" : ""
+                }`}
+              ></div>
+              <div
+                className={`${isActive4 ? "boat4" : "boat4v"} ${
+                  currentShip < 4 ? "bg-white" : ""
+                }`}
+              ></div>
+              <div
+                className={`${isActive3 ? "boat3" : "boat3v"} ${
+                  currentShip < 3 ? "bg-white" : ""
+                }`}
+              ></div>
+              <div
+                className={`${isActive2 ? "boat2" : "boat2v"} ${
+                  currentShip < 2 ? "bg-white" : ""
+                }`}
+              ></div>
             </div>
           </div>
         </div>
       )}
-      <div
-        className="rbtn"
-        style={{ justifyContent: "center", alignItems: "center" }}
-      >
-        <Button
-          // className="readybtn"
-          onClick={() => {
-            setUserBoatsReady(true);
-            sendBoatsReady(userData);
-          }}
-        >
-          Ready
-        </Button>
+      <div>
         <h6>{`You're Battling in Room: ${room}`}</h6>
       </div>
       {/* conditional render for active play */}
-
       <h5 className="title">Score Board</h5>
-
       <div className="shell2">
         <div className="ht-ms">
           <div>Hits: {userHit}</div>
           <div>Misses: {misses}</div>
           <div>Opponent Hits: {oppHit}</div>
         </div>
-
         <div className="shipbox">
           <div>
             <img
@@ -188,5 +136,4 @@ function ScoreBoard({
     </>
   );
 }
-
 export default ScoreBoard;
