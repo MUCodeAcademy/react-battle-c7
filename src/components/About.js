77029dc9e97aa54bad87a597727dd1d88c3c21c6
Rhonda Warner <sunflair_2000@yahoo.com>
2021-08-20 13:27:19 -0500

    styling complete. Need About text changed

--- src/components/About.js
+++ src/components/About.js
@@ -10,15 +10,15 @@ export default function About() {
         backgroundImage: "url(./assets/7battleA.png)",
         height: "100vh",
         zIndex: "-1",
-        textShadow: "1px 1px 0px #ffffff",
+        textShadow: "1px 1px 0px #darkblue",
       }}
     >
+<div className="aboutHead"></div>
+
       <Container>
-        <p>
-          {/* Let's not use the strong element, and use css styling instead */}
-          <strong>
-            Welcome to Lucky 7 Battleship! Created by the 7th cohort of Midland
-            Code Academy. Sign up and Login or if you’re already signed up, log
+<div className="aboutBody">        
+            Welcome to Lucky 7 Water Wars! Created by the 7th cohort of Midland
+            Code Academy. <br/>Sign up and Login or if you’re already signed up, log
             in. Select start game to retrieve a play code or join game to enter
             a code from the host. Select the ship you want to place on your grid
             and do this for all four ships varying in length from 2 to 5.
@@ -27,8 +27,27 @@ export default function About() {
             hit or miss. White peg is a miss. Red peg is a hit. A total of 14
             hits wins the game. Keep watch on the Scoreboard for your stats. Do
             you feel lucky?
-          </strong>
-        </p>
+
+
+<div style={{marginTop: "2em"}}></div>
+Signed, <br/>
+The Lucky 7, a.k.a.<br/>
+
+<a href="https://github.com/MUCodeAcademy/react-battle-c7">Cohort 7</a>
+
+<div className="about7">
+<a href="https://github.com/trevorrjacobson" target="_blank" rel="noreferrer noopener">Trevor Jacobson</a><br/>
+<a href="https://github.com/DarcyJorgensen" target="_blank" rel="noreferrer noopener">Darcy Jorgensen</a>  <br/>
+<a href="https://github.com/Zachary-Roland" target="_blank" rel="noreferrer noopener">Zachary Roland</a>  <br/>
+<a href="https://github.com/miketruax" target="_blank" rel="noreferrer noopener">Mike Truax </a> (Instructor)<br/>
+<a href="https://github.com/Sunflair0" target="_blank" rel="noreferrer noopener">Rhonda Warner</a>  <br/>
+<a href="https://github.com/keithawess" target="_blank" rel="noreferrer noopener">Keith Wess </a><br/>
+<a href="https://github.com/MichaelAZimmerman" target="_blank" rel="noreferrer noopener">Michael Arron Zimmerman</a><br/>
+</div>
+
+
+
+         </div>
       </Container>
     </div>
   );
